d282c33783c1285e3c3f4387d64eb24c03c49b10
divyanshi-agr <divsagr3@gmail.com>
2020-03-30 13:50:59 +0530

    changes in server.js

--- server.js
+++ server.js
@@ -41,11 +41,3 @@ app.listen(3000, () => {
     console.log('app is running on port 3000');
 });
 
-/*
-/ --> res = this is working
-/signin --> POST = success/fail
-/register --> POST = user
-/profile/:userId --> GET = user
-/image --> PUT --> user/count
-
-*/
